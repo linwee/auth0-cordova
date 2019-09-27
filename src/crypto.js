93c6c483f7114a772de1837b30b395ef37141bd2
Marjory <marjory.gaillot@gmail.com>
2019-09-27 15:46:26 +0200

    migrate crypto to crypto-js

--- src/crypto.js
+++ src/crypto.js
@@ -1,18 +1,25 @@
-var crypto = require('crypto');
+var CryptoJS = require('crypto-js');
 
 function base64UrlSafeEncode(string) {
-  return string.toString('base64')
+  /*return string.toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '');*/
+
+  return CryptoJS.enc.Base64.stringify(string)
         .replace(/\+/g, '-')
         .replace(/\//g, '_')
         .replace(/=/g, '');
 }
 
 function sha256(buffer) {
-  return crypto.createHash('sha256').update(buffer).digest();
+  //return crypto.createHash('sha256').update(buffer).digest();
+  return CryptoJS.SHA256(buffer);
 }
 
 exports.generateProofKey = function generateProofKey() {
-  var codeVerifier = base64UrlSafeEncode(crypto.randomBytes(32));
+  //var codeVerifier = base64UrlSafeEncode(crypto.randomBytes(32));
+  var codeVerifier = base64UrlSafeEncode(CryptoJS.lib.WordArray.random(32));
   var codeChallenge = base64UrlSafeEncode(sha256(codeVerifier));
   return {
     codeVerifier: codeVerifier,
@@ -21,5 +28,6 @@ exports.generateProofKey = function generateProofKey() {
 };
 
 exports.generateState = function generateState() {
-  return base64UrlSafeEncode(crypto.randomBytes(32));
+  //return base64UrlSafeEncode(crypto.randomBytes(32));
+  return base64UrlSafeEncode(CryptoJS.lib.WordArray.random(32));
 };
